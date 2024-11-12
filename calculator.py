697a986440e87cccbbda31c4fb3ee3d427a946e0
aftab <aftab111@gmail.com>
2024-11-12 16:09:37 +0530

    v.1.1

--- calculator.py
+++ calculator.py
@@ -1,19 +1,23 @@
+print("*************CALCULATOR*************")
 operator = input ("Enter an oprator(+ - * /):")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 num3 = float(input("Enter the 3rd number: "))
 
 if operator == "+":
-    result = num1 + num2
+    result = num1 + num2 + num3
     print(round(result, 3))
 elif operator == "-":
-    result = num1 - num2
+    result = num1 - num2 - num3
     print(round(result, 3))
 elif operator =="*":
-    result = num1 * num2
+    result = num1 * num2 * num3
     print(round(result, 3))
 elif operator == "/":
-    result = num1 / num2
+    result = num1 / num2 / num3
     print(round(result, 3))
 else:
     print (f"{operator} is not a valid oprator")
+print("******THANKS******")
+a=input("by the way what is you name:")
+print(a,"thanks to use i will wait for you")
---
+++ class1.py
@@ -0,0 +1,6 @@
+# this is example of comment
+a = input("Enter Your Name : ")
+b = input("Enter Your Sirname :")
+c = int(input("Enter Your Age : "))
+print(" Your age is : " + a + " Your Sir name is " + b + "your age is " + str(c))
+print("your age after 15 years will be " + str((c + 15)))
